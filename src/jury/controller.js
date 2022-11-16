dd2861b5b753160fbbbb9752466ccc7422c9ef5a
MaxencePaulin <maxence.paulin@edu.univ-fcomte.fr>
2022-11-16 14:24:18 +0100

    add condition

--- src/jury/controller.js
+++ src/jury/controller.js
@@ -4,12 +4,18 @@ import queriesSalle from '../salle/queries.js';
 
 const addJury = (req, res) => {
     const {nomjury, idsalle} = req.body;
+    if (!idsalle || idsalle === "") {
+        return res.status(400).send({success: 0, data: "Veuillez insérer un identifiant de salle"});
+    }
     pool.query(queriesSalle.getSalleById, [idsalle], (error, results) => {
         if (error) {
             return res.status(400).send({success: 0, data: error});
         }
         if (results.rowCount === 0) {
             return res.status(400).send({success: 0, data: "La salle n'existe pas"});
+        }
+        if (!nomjury || nomjury === "") {
+            return res.status(400).send({success: 0, data: "Veuillez insérer un nom de jury"});
         }
         pool.query(queries.getJuryByName, [nomjury], (error, results) => {
             if (error) {
